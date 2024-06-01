6dafd8849c3d1d65ff838c2b91f0c60fde7746f7
Sawant, Sreyas Anil <srsawant@iu.edu>
2024-06-01 00:16:19 -0400

    Pwc added

--- src/portfolio.js
+++ src/portfolio.js
@@ -18,6 +18,17 @@ export const portfolio = {
     },
     experiences: [
         {
+            company: "PwC Power BI Job Simulation on Forage | Remote",
+            position: "Data Analyst | May 2024",
+            projects: [],
+            work: [
+                "Completed a job simulation where I strengthened my PowerBI skills to better understand clients and their data visualisation needs.",
+                "Demonstrated expertise in data visualization through the creation of Power BI dashboards that effectively conveyed KPIs, showcasing the ability to respond to client requests with well-designed solutions.",
+                "Strong communication skills reflected in the concise and informative email communication with engagement partners, delivering valuable insights and actionable suggestions based on data analysis.",
+                "Leveraged analytical problem-solving skills to examine HR data, particularly focusing on gender-related KPIs, and identified root causes for gender balance issues at the executive management level, highlighting a commitment to data-driven decision-making."
+            ]
+        },
+        {
             company: "Indiana University - Bloomington | Indiana, US",
             position: "Research Assistant | Jun 2023 - Aug 2023",
             projects: [],
